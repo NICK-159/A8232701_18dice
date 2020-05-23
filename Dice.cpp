73cca18561e82da5a1ff658062c822b711a36cb3
鍾宇軒 <55938579+NICK-159@users.noreply.github.com>
2020-05-24 00:08:06 +0800

    Add files via upload

--- Dice.cpp
+++ Dice.cpp
@@ -1,87 +1,27 @@
 #include "Dice.h"
-#include <iostream>
-using namespace std;
 
-Dice::Dice(int * a)
-{
-	for (int i = 0; i < 4;i++)
-	{
-		dice[i] = a[i];
-	}
+Dice::Dice() {
+	money = 100;
+	point = 0;
 }
-
-int Dice::rule()
-{
-	for (int k = 0; k < 6;k++)
-	{
-		time[k] = 0;
-	}
-	for (int i = 0; i < 4;i++)
-	{
-		if (dice[i] > 6 || dice[i] < 1)
-		{
-			return false; //當點數不在骰子範圍時
-		}
-		for (int j = 0; j < 6;j++)
-		{
-			if (dice[i] == j + 1)
-			{
-				time[j]++;  //取的點數出現次數
-			}
-		}
-	}
-	for (int i = 0; i < 6;i++)
-	{
-		if (time[i] == 2 || time[i] == 4)
-		{
-			break;
-		}
-		if (i == 5)
-		{
-			return false;   //當未出現2+1+1或2+2或4等組合時
-		}
-	}
-
-	return true;
-}
-
-void Dice::setpoint()
-{
-	int p = 7, total = 0;
-	for (int i = 0; i < 6;i++)
-	{
-
-		if (time[i] == 4)
-		{
-			A = 13;  //當非4顆點數相同時，最大點數為12，故暫設為13
-		}
-
-		else if (time[i] == 2)
-		{
-			if (i + 1 < p)
-			{
-				p = i + 1; // 取得兩顆點數相同的點數(最小相同)
-			}
-		}
-	}
-	for (int i = 0; i < 4;i++)
-	{
-		total = total + dice[i];
-	}
-	A = (total - 2 * p);  //無論是否為2+2組合或2+1+1組合，皆減掉最小相同值
+void Dice::random() {
 
 }
+void Dice::setDice() {
 
-int Dice::getpoint() const
-{
-	return A;
 }
+string Dice::getDice() {
 
-void Dice::output()
-{
-	for (int i = 0; i < 4;i++)
-	{
-		cout << dice[i] << " ";
-	}
-	cout << A << endl;  //輸出組合及大小
+}
+bool Dice::tomanynumbers() {
+
+}
+int Dice::getCouple() {
+
+}
+int Dice::getPoint() {
+
+}
+int Dice::getWin() {
+
 }
