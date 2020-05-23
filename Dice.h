c28996e7eecc6847ff2a502395aa66b9ff851672
鍾宇軒 <55938579+NICK-159@users.noreply.github.com>
2020-05-23 23:54:42 +0800

    Add files via upload

--- Dice.h
+++ Dice.h
@@ -1,17 +1,33 @@
 #ifndef DICE_H
 #define DICE_H
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+
 
 class Dice
 {
 public:
-	Dice(int *a);  //設定點數
-	int rule();     //判斷是否符合規則
-	void setpoint();   //組合大小
-	int getpoint() const;   //取得組合大小
-	void output();      //輸出雙方點數
+	Dice(); //初始化金錢、分數
+	void random(); //打亂
+	void setDice(); //骰骰子
+	string getDice(); //取得骰的骰子
+	bool tomanynumbers(); //是否有至少一對骰子不能三個一樣
+	int getCouple(); //取得幾對骰子
+	int getPoint(); //取的目前分數
+	int getWin(); //判斷輸贏
+
 private:
-	int dice[4];   //點數
-	int time[6] = { 0 };  //數字出現的次數
-	int A;  //組合大小
+	int dice[4]; //骰子
+	int point; //分數
+	int money; //錢?賭金?
+	static int A;
+
 };
+
+
 #endif // !DICE_H
